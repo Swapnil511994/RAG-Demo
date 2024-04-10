3ab1c16fc2574da576abf62e9c75f400bc489fcb
Swapnil Vishwakarma <swapnil.vishwakarma@vertisystem.com>
2024-04-10 15:33:23 +0530

    Created a demo API for query processing

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import express from "express";
+import bodyParser from "body-parser";
+
 import { Document, VectorStoreIndex, SimpleDirectoryReader } from "llamaindex";
 
 const documents = await new SimpleDirectoryReader().loadData({
@@ -10,8 +13,30 @@ const documents = await new SimpleDirectoryReader().loadData({
 const index = await VectorStoreIndex.fromDocuments(documents);
 
 const queryEngine = index.asQueryEngine();
-const response = await queryEngine.query({
-  query: "what is author's opinion on job?",
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ status: true, message: "Server is running" });
 });
 
-console.log(response.toString());
+app.post("/", async (req, res) => {
+  const queryText = req.body.query;
+  const response = await queryEngine.query({
+    query: queryText,
+  });
+  if (response?.toString().length > 0) {
+    return res.json({
+      status: true,
+      message: "Query Successful",
+      data: response.toString(),
+    });
+  } else {
+    res.json({ status: false, message: "Query Failed" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port: 3000");
+});
